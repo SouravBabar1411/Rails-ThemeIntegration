ca3c1d27d9cfb3497a540e389aef5d0e03491393
sourav1411 <babarsourav12@gmail.com>
2022-02-22 13:33:35 +0530

    added authentication for post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :set_sport
   before_action :set_post, only: [:update, :show, :destroy]
   
