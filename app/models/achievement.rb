e77312228ca20b5659bfc35f015e73c8a2f1d87f
sourav1411 <babarsourav12@gmail.com>
2022-02-22 16:14:44 +0530

    added validation for achievement modle

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -6,6 +6,10 @@
 #string: "medal"
 
 class Achievement < ApplicationRecord
+    #validation
+    validate :award, :presence: true
+    validate :medal, :presence: true
+
     #assosiation
     belongs_to :user
 end
