9883e3e34ee754baed59260bf5d31f26812eab7a
sourav1411 <babarsourav12@gmail.com>
2022-03-25 15:47:06 +0530

    added before action filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :login_require
+  
   ## Pagination Page Number
   def page
     @page ||= params[:page] || 20
@@ -7,5 +9,15 @@ class ApplicationController < ActionController::Base
   ## Pagination Per Page Records
   def per_page
     @per_page ||= params[:per_page] || 20
+  end
+  protected
+  def login_require
+    unless logged_in?
+      flash[:error] = "You must have to login first"
+    end
+  end
+
+  def logged_in?
+    authenticate_user!
   end
 end
